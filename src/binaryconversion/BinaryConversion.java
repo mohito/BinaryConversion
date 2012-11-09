af49202d25cc7557e89595a6cdded4741c4ef4fd
mohito <paffcio1@gmail.com>
2012-11-09 12:29:25 +0100

    Test

---
+++ src/binaryconversion/BinaryConversion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binaryconversion;
+
+/**
+ *
+ * @author Paweł
+ */
+public class BinaryConversion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frame a=new Frame();
+        a.setVisible(true);
+    }
+}
